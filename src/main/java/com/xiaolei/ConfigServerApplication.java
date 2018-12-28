0caa9f14c89f779a916900136fdaa33dfe266af2
xiaolei002 <2228588989@qq.com>
2018-12-28 16:23:16 +0800

    更新

--- src/main/java/com/xiaolei/ConfigServerApplication.java
+++ src/main/java/com/xiaolei/ConfigServerApplication.java
@@ -3,13 +3,14 @@ package com.xiaolei;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@EnableEurekaServer
 @EnableConfigServer
+@EnableEurekaClient
 @SpringBootApplication
 public class ConfigServerApplication {
      public static void main(String[] args) {
          SpringApplication.run(ConfigServerApplication.class, args);
      }
+
 }
